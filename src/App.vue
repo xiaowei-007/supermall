15c5b18291e59f4dac47a8f8bddb068d1561eb10
xiaowei-007 <302929314@qq.com>
2020-06-25 01:13:24 +0800

    修改了一些信息

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div>这是我的第一个vue项目</div>
+    <div>项目内容</div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
-
 </style>
