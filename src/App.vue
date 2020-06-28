19f6b3aa67c7b1796c1e992baf1ed59f7bc886ce
xiaowei-007 <302929314@qq.com>
2020-06-28 15:05:55 +0800

    引用elementUI

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <div>这是我的第一个vue项目</div>
-
-    <div>这是我的第一个vue项目</div>
-    <div>这是我的第一个vue项目</div>
-    <div>这是我的第一个vue项目</div>
-    <div>这是我的第一个vue项目</div>
+    <home></home>
   </div>
 </template>
 
 <script>
+import Home from './components/Home.vue'
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    Home
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue'
+
+/* import ViewUI from 'view-design';
+//import 'view-design/dist/styles/iview.css';
+
+Vue.use(ViewUI); */
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
